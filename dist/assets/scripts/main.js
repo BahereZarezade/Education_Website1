c55e1204446cbe1dd1a4c272b2f3b693d64b3a98
Bahere Zarezade <b.zarezade@gmail.com>
2021-08-09 20:12:09 +0430

    add adv section.

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -1,45 +1,42 @@
-$('#main_slider').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
-        }
-    }
-})
+$("#main_slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 1,
+      nav: true,
+      loop: true,
+    },
+  },
+});
 
-$('#multi_slider').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:1,
-            nav:false
-        },
-        1000:{
-            items:4,
-            nav:true,
-            loop:false
-        }
-    }
-})
-
-
-
-
+$("#multi_slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+      loop: true,
+    },
+  },
+});
