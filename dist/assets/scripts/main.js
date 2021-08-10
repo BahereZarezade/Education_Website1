bb94919e6d263755269994f8cbeecf5f0135c158
Bahere Zarezade <b.zarezade@gmail.com>
2021-08-10 16:38:57 +0430

    add course section.

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -40,3 +40,24 @@ $("#multi_slider").owlCarousel({
     },
   },
 });
+
+$("#course_slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+      loop: true,
+    },
+  },
+});
